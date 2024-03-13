4ea05a4a3e35cc1a7de919c4784e5402af6c506e
Chan Jin Yee <chan1992241@gmail.com>
2024-03-13 10:21:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	log.Println("Welcome for Jenkins POC Demo 5")
+	log.Println("Welcome for Jenkins POC Demo 6")
 	ticker := time.NewTicker(60 * time.Second)
 	for range ticker.C {
 		log.Println("Hello, CCE Kubernetes！I'm from Jenkins CI！")
